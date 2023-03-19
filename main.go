330593db92c406bafc7309b4ba992c09e61b8127
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2023-03-19 11:00:31 +0500

    feat: add range post route

--- main.go
+++ main.go
@@ -20,6 +20,12 @@ type CashBody struct {
 	Note    string  `json:"note"`
 }
 
+type RangeBody struct {
+	APIKey string `json:"api_key" binding:"required"`
+	Detail string `json:"detail"`
+	Note   string `json:"note"`
+}
+
 func main() {
 	// Database instance
 	db := db.CreateDB()
@@ -73,5 +79,53 @@ func main() {
 			"uuid":    _uuid,
 		})
 	})
+
+	r.POST("/ranges", func(ctx *gin.Context) {
+		// Get request body
+		var body RangeBody
+		if err := ctx.BindJSON(&body); err != nil {
+			logger.Errorf("request body wrong %v", err)
+			ctx.JSON(400, gin.H{
+				"error":   err.Error(),
+				"message": "Request body invalid",
+			})
+			return
+		}
+
+		// Find the client with the given key
+		var client string
+		err := db.QueryRow(context.Background(), "SELECT name FROM clients WHERE api_key = $1", body.APIKey).Scan(&client)
+		if err != nil {
+			logger.Errorf("api key search error %v", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error":   err,
+				"message": "Client hasn't been found",
+			})
+			return
+		}
+
+		// Insert request to database
+		_uuid := uuid.New().String()
+		sqlStatement := `
+		INSERT INTO ranges (uuid, created_at, updated_at, client, detail, note)
+		VALUES ($1, $2, $3, $4, $5, $6)
+		`
+		_, err = db.Exec(context.Background(), sqlStatement, _uuid, time.Now(), time.Now(), client, body.Detail, body.Note)
+		if err != nil {
+			logger.Errorf("database save error %v", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error":   err,
+				"message": "Couldn't save into cashes",
+			})
+			return
+		}
+
+		// Send success result
+		ctx.JSON(201, gin.H{
+			"message": "Successfully saved into database",
+			"uuid":    _uuid,
+		})
+	})
+
 	r.Run()
 }
