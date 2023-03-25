cbfe520eaa9e38378dd4c7f49bb51345368ddab2
milkaxq <extradamage2@gmail.com>
2023-03-25 19:54:58 +0500

    added csahes and ranges

--- main.go
+++ main.go
@@ -26,6 +26,22 @@ type RangeBody struct {
 	Note   string `json:"note"`
 }
 
+type RangeBodyResponse struct {
+	Detail    string    `json:"detail"`
+	Note      string    `json:"note"`
+	Client    string    `json:"client"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type CashBodyResponse struct {
+	Amount    float64   `json:"amount" binding:"required"`
+	Detail    string    `json:"detail"`
+	Note      string    `json:"note"`
+	Client    string    `json:"client"`
+	Contact   string    `json:"contact"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
 func main() {
 	// Database instance
 	db := db.CreateDB()
@@ -124,6 +140,82 @@ func main() {
 		ctx.JSON(201, gin.H{
 			"message": "Successfully saved into database",
 			"uuid":    _uuid,
+		})
+	})
+	r.GET("/ranges", func(ctx *gin.Context) {
+		var rangeBody RangeBodyResponse
+		var rangeBodies []RangeBodyResponse
+		sqlStatement := `SELECT r.detail, r.note, r.client, r.created_at, r.updated_at FROM ranges r ORDER BY r.created_at DESC LIMIT $1 OFFSET $2;`
+		rows, err := db.Query(context.Background(), sqlStatement, 10, 0)
+		if err != nil {
+			logger.Errorf("ranges search error %v", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error":   err,
+				"message": "Something went wrong",
+			})
+			return
+		}
+		for rows.Next() {
+			err := rows.Scan(&rangeBody.Detail, &rangeBody.Note, &rangeBody.Client, &rangeBody.CreatedAt)
+			if err != nil {
+				logger.Errorf("Scan error %v", err)
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error":   err,
+					"message": "Scan error",
+				})
+				return
+			}
+			rangeBodies = append(rangeBodies, rangeBody)
+		}
+		ctx.JSON(200, gin.H{
+			"message": "Successfully get ranges",
+			"ranges":  rangeBodies,
+		})
+	})
+
+	r.GET("/cashes", func(ctx *gin.Context) {
+		var cashBody CashBodyResponse
+		var cashBodies []CashBodyResponse
+		sqlStatement := `
+		SELECT
+		MAX(client),
+		MAX(contact),
+		SUM(amount),
+		MAX(detail),
+		MAX(created_at)
+	FROM
+		cashes
+	WHERE
+		created_at > $1
+		AND created_at < $2
+	GROUP BY
+		contact
+	ORDER BY
+		MAX(created_at) DESC;`
+		rows, err := db.Query(context.Background(), sqlStatement, "2023-03-19", "2023-03-20")
+		if err != nil {
+			logger.Errorf("cashes search error %v", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error":   err,
+				"message": "Something went wrong",
+			})
+			return
+		}
+		for rows.Next() {
+			err := rows.Scan(&cashBody.Client, &cashBody.Contact, &cashBody.Amount, &cashBody.Detail, &cashBody.CreatedAt)
+			if err != nil {
+				logger.Errorf("Scan error %v", err)
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error":   err,
+					"message": "Scan error",
+				})
+				return
+			}
+			cashBodies = append(cashBodies, cashBody)
+		}
+		ctx.JSON(200, gin.H{
+			"message": "Successfully get cashes",
+			"ranges":  cashBodies,
 		})
 	})
 
